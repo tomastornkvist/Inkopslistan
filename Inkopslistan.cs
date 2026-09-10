22b1aa10c4a876a2102fa445881c8b5146e0504d
tomastornkvist <tomas.tornkvist@gmail.com>
2026-09-10 12:35:16 +0200

    Added some functionality to be able to run the program

--- Inkopslistan.cs
+++ Inkopslistan.cs
@@ -1,26 +1,91 @@
 List<string> items = new();
 List<int> prices = new();
 
+bool firstTime = true;
+
+// An eternal loop that keeps the program running
 while (true)
+{
+    int index;
+
+    PrintItems();
+
+    PrintUsage();
+    string input = Console.ReadLine()!;
+
+    if (input.ToLower().Equals("avsluta"))
+        break;
+
+    if (input.ToLower().Equals("dyrast"))
+        PrintMostExpensive();
+
+    else if (int.TryParse(input, out index))
+        RemoveItem(index);
+
+    else
+    {
+        int price;
+
+        while (true)
+        {
+            Console.Write("Ange varans pris: ");
+
+            if (int.TryParse(Console.ReadLine(), out price))
+            {
+                AddItem(input, price);
+                break;
+            }
+            else
+                Console.Write("Priset måste vara i hela kronor. ");
+        }
+    }
+}
+
+void PrintItems()
 {
     for (int i = 0; i < items.Count; i++)
         Console.WriteLine($"{i + 1}. {items[i]} - {prices[i]}");
+}
 
-    string input = Console.ReadLine()!;
-
-    if (input.ToLower().Equals("dyrast"))
+void PrintUsage()
+{
+    if (!firstTime)
     {
-        int selected = -1;
-
-        for (int i = 0; i < prices.Count; i++)
-        {
-            if (selected < 0)
-                selected = i;
-            else if (prices[i] > prices[selected])
-                selected = i;
-        }
-
-        if (selected >= 0)
-            Console.WriteLine($"Dyraste varan är {selected + 1}. {items[selected]} - {prices[selected]}");
+        Console.WriteLine("");
+        Console.WriteLine("---------------------------------------------");
     }
+    else
+        firstTime = false;
+
+    Console.WriteLine("Skriv namnet på en ny vara för att lägga till i inköpslistan.");
+    Console.WriteLine("Numret på en befintlig vara tar bort den.");
+    Console.WriteLine("DYRAST visar dyraste varan.");
+    Console.WriteLine("AVSLUTA stänger ner programmet.");
+    Console.Write("Vad vill du göra: ");
+}
+
+void PrintMostExpensive()
+{
+    int selected = -1;
+
+    for (int i = 0; i < prices.Count; i++)
+    {
+        if (selected < 0)
+            selected = i;
+        else if (prices[i] > prices[selected])
+            selected = i;
+    }
+
+    if (selected >= 0)
+        Console.WriteLine($"Dyraste varan är {selected + 1}. {items[selected]} - {prices[selected]}");
+}
+
+void RemoveItem(int index)
+{
+
+}
+
+void AddItem(string input, int price)
+{
+
 }
